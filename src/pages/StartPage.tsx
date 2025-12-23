c6a1390bc8764411a1d40e34d7019977e7120ddb
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-12-23 11:41:12 +0100

    feat: Add navigation to TimerSettings from StartPage-btn

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,6 +1,12 @@
+import { useNavigate } from "react-router";
 import tomoicon from "../assets/tomoicon.png";
 
 export const StartPage = () => {
+    const navigate = useNavigate(); 
+   
+    const handleStart = () => {
+        navigate("/settings");
+    };
 
     return (
         <> 
@@ -15,7 +21,7 @@ export const StartPage = () => {
 
                     <p className="mt-3">Klicka på knappen nedan för att starta din timer</p>
                     <button
-                    // onClick={handleStart}
+                    onClick={handleStart}
                     className="bg-red transition border-2 border-border text-secondary py-3 px-6 rounded-xl">Time to Focus!</button>
                     <img src={tomoicon} alt="decorative tomato mascot" className="w-20 mx-auto"/>
 
