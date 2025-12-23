18fdee64ef4d712f284f01b74921641de144978f
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-12-23 11:38:45 +0100

    feat: Add startPage layout and content draft

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,9 +1,27 @@
+import tomoicon from "../assets/tomoicon.png";
+
 export const StartPage = () => {
 
     return (
         <> 
-        <h1 className="text-secondary text-5xl">Rubrik</h1>
-        <p>Nu kör vi!</p>
+        <div>
+            <main>
+                <h1 className="sr-only">TomoFocus, a timer-focus app</h1>
+                <div className="m-7 border-2 border-border rounded-2xl p-6  bg-background flex flex-col gap-6">
+                    <p>TomoFocus hjälper dig att arbeta fokuserat med Pomodoro-tekniken.
+                    Arbeta i korta fokuserade pass med regelbundna pauser för bättre
+                    koncentration och mindre stress. ELLER SOM DU STÄLLER IN
+                    </p>
+
+                    <p className="mt-3">Klicka på knappen nedan för att starta din timer</p>
+                    <button
+                    // onClick={handleStart}
+                    className="bg-red transition border-2 border-border text-secondary py-3 px-6 rounded-xl">Time to Focus!</button>
+                    <img src={tomoicon} alt="decorative tomato mascot" className="w-20 mx-auto"/>
+
+                </div> 
+            </main>
+        </div>
         </>
     )
 }
