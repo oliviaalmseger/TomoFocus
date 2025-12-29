5bda99625fa8bf0832d44b6c4fb706cee4d09365
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-12-29 12:48:11 +0100

    feat: Add frame for TimerSettings page

--- src/pages/TimerSettings.tsx
+++ src/pages/TimerSettings.tsx
@@ -2,7 +2,33 @@ export const TimerSettings = () => {
 
     return (
         <> 
-        <p className="text-pink-400">TimerSettings</p>
+        <div>
+            <main>
+                <h1>Timer settings</h1>
+                <div>
+                    <button>Classic Pomodoro</button>
+                    <button>Last settings</button>
+                    <button>Customize timer</button>
+                </div>
+                <div>
+                    <label> Work interval (minutes)
+                        <input/>
+                    </label>
+                    <label> Break interval (minutes)
+                        <input/>
+                    </label>
+                    <label> Number of sets
+                        <input/>
+                    </label>
+                </div>
+
+                <div>
+                    <button>Go back</button>
+                    <button>Start timer</button>
+                </div>
+                
+            </main>
+        </div>
         </>
     )
 }
