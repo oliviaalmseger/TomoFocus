5f4deee25c53f76684fb04d6dfbdfc8fffa5b1f5
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2026-01-05 19:34:06 +0100

    style: Refine completePage, text and spacing

--- src/pages/CompletePage.tsx
+++ src/pages/CompletePage.tsx
@@ -8,19 +8,25 @@ export const CompletePage = () => {
     return (
         <> 
         <div className="flex justify-center px-4">
-            <section className="w-full max-w-[420px] flex flex-col items-center text-center gap-6 py-10">
-                <h1 className="text-2xl font-bold">
+            <section className="w-full max-w-[420px] flex flex-col items-center text-center py-10">
+                <h1 className="text-2xl font-bold mb-4">
                     Congratulations!
                 </h1>
                 <p>
-                    You completed all your Pomodoro sets. Great job staying focused! Blabla..
+                    You completed all your Pomodoro sets. 
+                </p>
+                <p>
+                    Great job staying focused!
+                </p>
+                <p className="mt-3">
+                    Ready for another round?
                 </p>
 
-                <div className="w-64 h-64 bg-background border-4 border-border rounded-2xl flex items-center justify-center">
-                    <img src={completeImage} alt="Break session illustration" className="w-full h-full object-contain"/>
+                <div className="w-64 h-64 bg-background border-4 border-border rounded-2xl flex items-center justify-center my-5">
+                    <img src={completeImage} alt="Celebrating tomato character" className="w-full h-full object-contain"/>
                 </div>
 
-                <button onClick={() => navigate("/")} className="mt-4 bg-primary hover:brightness-110 text-third rounded-xl px-6 py-3 cursor-pointer">
+                <button onClick={() => navigate("/")} className="my-4 bg-primary hover:brightness-110 text-third rounded-xl px-6 py-3 cursor-pointer">
                     Return to home
                 </button>
             </section>
